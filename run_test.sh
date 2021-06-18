58625a2ac1ceb7e3bcfa69234ea741a45b76ea46
shanek16 <shanesw16@gmail.com>
2021-06-18 11:31:00 +0900

    v4 added

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-python3 test_model.py cfg/dagger_leader_v2.cfg
-python3 test_model.py cfg/dagger_leader1_v2.cfg
-python3 test_model.py cfg/dagger_leader2_v2.cfg
+# python3 test_model.py cfg/dagger_leader_v2.cfg
+# python3 test_model.py cfg/dagger_leader1_v2.cfg
+# python3 test_model.py cfg/dagger_leader2_v2.cfg
+for i in {1..10..1}
+do
+    python3 test_model.py cfg/dagger_leader_v4.cfg $i
+done
--- test_model.py
+++ test_model.py
@@ -11,34 +11,17 @@ from learner.state_with_delay import MultiAgentStateWithDelay
 from learner.gnn_dagger import DAGGER
 
 
-def test(args, actor_path, render=False):
+def test(args, actor_path, render=True):
     # initialize gym env
     env_name = args.get('env')
     print('env_name= ', env_name)
     env = gym.make(env_name)
     if isinstance(env.env, gym_flock.envs.flocking.FlockingRelativeEnv):
-        env.env.params_from_cfg(args)
-        
-    elif isinstance(env.env, gym_flock.envs.flocking.FlockingLeaderEnv):
-        env.env.params_from_cfg(args)
-    
-    elif isinstance(env.env, gym_flock.envs.flocking.FlockingLeaderEnv1):
-        env.env.params_from_cfg(args)
-    
-    elif isinstance(env.env, gym_flock.envs.flocking.FlockingLeaderEnv2):
-        env.env.params_from_cfg(args)
-
-    elif isinstance(env.env, gym_flock.envs.flocking.FlockingLeaderEnv_v2):
-        env.env.params_from_cfg(args)
-
-    elif isinstance(env.env, gym_flock.envs.flocking.FlockingLeaderEnv1_v2):
-        env.env.params_from_cfg(args)
-
-    elif isinstance(env.env, gym_flock.envs.flocking.FlockingLeaderEnv2_v2):
-        env.env.params_from_cfg(args)
-
-    elif isinstance(env.env, gym_flock.envs.flocking.FlockingLeaderEnv_v3):
-        env.env.params_from_cfg(args)
+        if env_name == 'FlockingLeader-v4':
+            m = sys.argv[2]
+            env.env.params_from_cfg(args, m)
+        else:
+            env.env.params_from_cfg(args)
 
     # use seed
     seed = args.getint('seed')
@@ -54,7 +37,7 @@ def test(args, actor_path, render=False):
     learner.load_model(actor_path, device)
 
     for _ in range(n_test_episodes):
-        print('\nepisode {}:'.format(_+1))
+        # print('\nepisode {}:'.format(_+1))
         episode_reward = 0
         state = MultiAgentStateWithDelay(device, args, env.reset(), prev_state=None)
         done = False
@@ -66,7 +49,7 @@ def test(args, actor_path, render=False):
             state = next_state
             if render:
                 env.render()
-        print('episode_reward:',episode_reward)
+        # print('episode_reward:',episode_reward)
     env.close()
 
 
@@ -80,14 +63,15 @@ def main():
     # actor_path = 'models/actor_FlockingRelative-v0_dagger_k3'
     # actor_path = 'models/actor_FlockingRelative-v0_dagger_k1'
     # actor_path = 'models/actor_FlockingRelative-v0_dagger_k4'
-    actor_path = 'models/actor_FlockingLeader-v2_dagger_leader_v2_k4' #works best
+    # actor_path = 'models/actor_FlockingLeader-v2_dagger_leader_v2_k4'
     # actor_path = 'models/actor_FlockingLeader-v2_dagger_leader_v2_k4_2pi'
+    actor_path = 'models/actor_FlockingLeader-v2_dagger_leader_v2_k4_r5'#works best
 
 
     if config.sections():
         for section_name in config.sections():
             if not printed_header:
-                print(config[section_name].get('header'))
+                # print(config[section_name].get('header'))
                 printed_header = True
 
             test(config[section_name], actor_path)
